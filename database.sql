b3e9c9b0ddbbfc156c56c1e8873486af26bef3ad
rondanen <Ron@MacBook-Pro-de-Ron-Danenberg.local>
2015-02-28 16:30:54 +0000

    Database

---
+++ database.sql
@@ -0,0 +1,120 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.7
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost
+-- Généré le: Sam 28 Février 2015 à 17:30
+-- Version du serveur: 5.5.29
+-- Version de PHP: 5.4.10
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Base de données: `quickstart`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `favorite_shops`
+--
+
+CREATE TABLE `favorite_shops` (
+  `favorite_shop_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `datetime_created` datetime NOT NULL,
+  PRIMARY KEY (`favorite_shop_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `selection_id` int(11) NOT NULL,
+  `is_received` tinyint(4) NOT NULL,
+  `datetime_ordered` datetime NOT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_bin NOT NULL,
+  `shop_id` int(11) NOT NULL,
+  `picture` text COLLATE utf8_bin NOT NULL,
+  `datetime_added` datetime NOT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `products_configurations`
+--
+
+CREATE TABLE `products_configurations` (
+  `product_configuration_id` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_bin NOT NULL,
+  `price` double(10,2) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `datetime_added` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `selections`
+--
+
+CREATE TABLE `selections` (
+  `selection_id` int(11) NOT NULL AUTO_INCREMENT,
+  `shop_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_configuration_id` int(11) NOT NULL,
+  `datetime_edited` datetime NOT NULL,
+  PRIMARY KEY (`selection_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `shops`
+--
+
+CREATE TABLE `shops` (
+  `shop_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `latitude` varchar(25) COLLATE utf8_bin NOT NULL,
+  `longitude` varchar(25) COLLATE utf8_bin NOT NULL,
+  `address` text COLLATE utf8_bin NOT NULL,
+  `postcode` varchar(10) COLLATE utf8_bin NOT NULL,
+  `city` varchar(100) COLLATE utf8_bin NOT NULL,
+  `datetime_registered` datetime NOT NULL,
+  PRIMARY KEY (`shop_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `last_name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `email` varchar(255) COLLATE utf8_bin NOT NULL,
+  `password` varchar(200) COLLATE utf8_bin NOT NULL,
+  `datetime_registered` datetime NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
