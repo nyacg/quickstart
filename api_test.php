ae886221d090a1880e67d5446571da2fcb1efd7c
rondanen <Ron@MacBook-Pro-de-Ron-Danenberg.local>
2015-02-28 18:14:56 +0000

    Api test

---
+++ api_test.php
@@ -0,0 +1,13 @@
+<?php
+/*
+*	Allow to test the API
+*
+*/
+?>
+
+<h1>API LOGIN</h1>
+<form method="post" action="api_login.php">
+	<label for="email">EMAIL</label><input type="submit" name="email" id="email" /><br />
+	<label for="password">PASSWORD</label><input type="password" name="password" id="password" /><br />
+	<button type="submit">SUBMIT</button>
+</form>
