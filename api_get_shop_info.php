d552263d81563cf3f5dfeb9ceb362e289a36a205
rondanen <Ron@MacBook-Pro-de-Ron-Danenberg.local>
2015-02-28 22:56:47 +0000

    API for Rob

---
+++ api_get_shop_info.php
@@ -0,0 +1,35 @@
+<?php
+/***********
+*
+*	Called to get the info on a shop
+*
+*
+************/
+include_once("functions.php");
+include_once("Shop.class.php");
+include_once("User.class.php");
+
+$array_values_json = array('result' => false);
+if (isset($_POST['user_id']) AND isset($_POST['shop_id']))
+{
+
+	$user_connected = new User($_POST['user_id']);
+	$shop_displayed = new Shop($_POST['shop_id']);
+	if ($user_connected->userExists() AND $shop_displayed->shopExists())
+	{
+
+		$array_values_json['result'] = true;
+		
+		$array_values_json['shop_id'] = $shop_displayed->getShopId();
+		$array_values_json['name'] = $shop_displayed->getName();
+		$array_values_json['latitude'] = $shop_displayed->getLatitude();
+		$array_values_json['longitude'] = $shop_displayed->getLongitude();
+		$array_values_json['address'] = $shop_displayed->getAddress();
+		$array_values_json['postcode'] = $shop_displayed->getPostCode();
+		$array_values_json['city'] = $shop_displayed->getCity();
+		
+	}
+}
+
+echo json_encode($array_values_json);
+?>
