92833ad9399084bb577fd116d108417f7d56ffc7
rondanen <Ron@MacBook-Pro-de-Ron-Danenberg.local>
2015-02-28 23:00:47 +0000

    Change API

--- api_get_selection.php
+++ api_get_selection.php
@@ -1,13 +1,14 @@
 <?php
 /***********
 *
-*	Called to get the products & their configurations
+*	Called to place an order
 *
 *
 ************/
 include_once("functions.php");
 include_once("Product.class.php");
 include_once("ProductConfiguration.class.php");
+include_once("Shop.class.php");
 include_once("User.class.php");
 
 $array_values_json = array('result' => false);
@@ -25,7 +26,19 @@ if (isset($_POST['user_id']))
 		$array_values_json['result'] = true;
 		
 		$array_values_json['shop_id'] = $shop_displayed->getShopId();
+		$array_values_json['shop_name'] = $shop_displayed->getName();
+		$array_values_json['shop_latitude'] = $shop_displayed->getLatitude();
+		$array_values_json['shop_longitude'] = $shop_displayed->getLongitude();
+		$array_values_json['shop_address'] = $shop_displayed->getAddress();
+		$array_values_json['shop_postcode'] = $shop_displayed->getPostCode();
+		$array_values_json['shop_city'] = $shop_displayed->getCity();
 		
+		$array_values_json['product_id'] = $product_displayed->getProductId();
+		$array_values_json['product_name'] = $product_displayed->getName();
+		
+		$array_values_json['product_configuration_id'] = $product_configuration_displayed->getProductConfigurationId();
+		$array_values_json['product_configuration_name'] = $product_configuration_displayed->getName();
+		$array_values_json['product_configuration_price'] = $product_configuration_displayed->getPrice();
 		
 	}
 }
--- api_get_shop_info.php
+++ api_get_shop_info.php
@@ -7,15 +7,13 @@
 ************/
 include_once("functions.php");
 include_once("Shop.class.php");
-include_once("User.class.php");
 
 $array_values_json = array('result' => false);
-if (isset($_POST['user_id']) AND isset($_POST['shop_id']))
+if (isset($_POST['shop_id']))
 {
 
-	$user_connected = new User($_POST['user_id']);
 	$shop_displayed = new Shop($_POST['shop_id']);
-	if ($user_connected->userExists() AND $shop_displayed->shopExists())
+	if ($shop_displayed->shopExists())
 	{
 
 		$array_values_json['result'] = true;
